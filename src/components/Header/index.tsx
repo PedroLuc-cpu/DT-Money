d1129995ea660a02414e91aee7b867ca87b8f3c9
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2023-05-23 21:16:39 -0300

    implementação: Estrutura visual - Criando um modal acessível

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import { HeaderContainer, HeaderContent, NewTransaction } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
+
 
 import logoImage from '../../assets/Logo.svg'
 
@@ -7,7 +9,18 @@ export function Header(){
        <HeaderContainer>
               <HeaderContent>
                      <img src={logoImage}/>
-                     <NewTransaction>Nova Transaçao</NewTransaction>
+                     <Dialog.Root>
+                            <Dialog.Trigger asChild>
+                                   <NewTransaction>Nova Transaçao</NewTransaction>
+                            </Dialog.Trigger>
+                            <Dialog.Portal>
+                                   <Dialog.Overlay/>
+                                   <Dialog.Content>
+                                          <Dialog.Title>Nova transação</Dialog.Title>
+                                          <Dialog.Close />
+                                   </Dialog.Content>
+                            </Dialog.Portal>
+                     </Dialog.Root>
               </HeaderContent>
        </HeaderContainer>
        )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import * as Dialog from '@radix-ui/react-dialog';
+
 
 export const HeaderContainer = styled.header`
 background: ${(props) => props.theme['gray-900']};
