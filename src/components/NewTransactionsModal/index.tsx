30f585751a9a468769b1dc2d69dae6cb80017da8
Pedro Lucas Santos <85002280+PedroLuc-cpu@users.noreply.github.com>
2023-05-24 21:11:29 -0300

    Commit: Estrutura visual - Criando radio button acessível

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -19,12 +19,12 @@ export function NewTransactionsModal() {
                                    <input type="text" placeholder='Categoria' required />
                                    
                                    <TransactionType>
-                                          <TransactionTypeButton variant='income'>
+                                          <TransactionTypeButton variant='income' value='income'>
                                                  <ArrowCircleUp size={24} />
                                                  Entrada
                                           </TransactionTypeButton>
 
-                                          <TransactionTypeButton variant='outcome'>
+                                          <TransactionTypeButton variant='outcome' value='outcome'>
                                                  <ArrowULeftDown size={24} />
                                                  Saída
                                           </TransactionTypeButton>
